bb1f2fc16a35a93597e3716779febb220f012c0e
root <root@Joffrey.localdomain>
2020-12-12 15:44:42 -0700

    updated html with placeholders

--- javascript.js
+++ javascript.js
@@ -1,1 +1,4 @@
-console.log('i am working');
+const wordChoices = ['handstand', 'child pose', 'corpse', 'warrior', 'happy baby', 'frog', 'pigeon', 'upward dog', 'crane', 'bow', 'eagle', 'wheel', 'boat', 'plow', 'cow face']
+let wordSpot = document.querySelector('.win');
+
+console.log(wordSpot);
