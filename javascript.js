346cf4daa4c34c2b8dc346003e8a1c4f983b2721
root <root@Joffrey.localdomain>
2020-12-13 20:20:22 -0700

    test

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ const dash = '_';
 
 
 function startGame() {
-    
+    console.log('did it work');
 }
 
 
@@ -18,7 +18,7 @@ document.onkeyup = function (e) {
     wordChoices[Math.floor(Math.random() * wordChoices.length)];
     console.log(computerGuess.length);
 
-    
+
 
 
     wordSpot.textContent = computerGuess;
