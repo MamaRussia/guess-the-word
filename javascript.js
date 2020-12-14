66eadd630dcbff0d251df9c17c2212ddaf6104ea
root <root@Joffrey.localdomain>
2020-12-13 20:15:12 -0700

    iterate through array and push word choice to id on page

--- javascript.js
+++ javascript.js
@@ -1,4 +1,25 @@
 const wordChoices = ['handstand', 'child pose', 'corpse', 'warrior', 'happy baby', 'frog', 'pigeon', 'upward dog', 'crane', 'bow', 'eagle', 'wheel', 'boat', 'plow', 'cow face']
-let wordSpot = document.querySelector('.win');
+let wordSpot = document.querySelector('#word-choice');
+const startBtn = document.querySelector('#start');
+let win = document.querySelector('#wins');
+const dash = '_';
 
-console.log(wordSpot);
+
+function startGame() {
+    
+}
+
+
+document.onkeyup = function (e) { 
+    let userGuess = e.key;
+    console.log(e.key);
+    console.log(wordSpot);
+    let computerGuess =
+    wordChoices[Math.floor(Math.random() * wordChoices.length)];
+    console.log(computerGuess.length);
+
+    
+
+
+    wordSpot.textContent = computerGuess;
+ }
