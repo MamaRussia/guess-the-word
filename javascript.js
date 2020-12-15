2a7e3ec92c46ee4538609bfd4a5e749cbbaaa513
joffrey <jaydru@gmail.com>
2020-12-14 17:48:45 -0700

    fixed eslint

--- javascript.js
+++ javascript.js
@@ -1,25 +1,45 @@
-const wordChoices = ['handstand', 'child pose', 'corpse', 'warrior', 'happy baby', 'frog', 'pigeon', 'upward dog', 'crane', 'bow', 'eagle', 'wheel', 'boat', 'plow', 'cow face']
-let wordSpot = document.querySelector('#word-choice');
+const wordChoices = [
+  'handstand',
+  'child pose',
+  'corpse',
+  'warrior',
+  'happy baby',
+  'frog',
+  'pigeon',
+  'upward dog',
+  'crane',
+  'bow',
+  'eagle',
+  'wheel',
+  'boat',
+  'plow',
+  'cow face',
+];
+const wordSpot = document.querySelector('#word-choice');
 const startBtn = document.querySelector('#start');
-let win = document.querySelector('#wins');
+const win = document.querySelector('#wins');
 const dash = '_';
 
-
 function startGame() {
-    console.log('did it work dues');
+  console.log('did it work dues');
 }
 
+console.log(wordChoices[4]);
 
-document.onkeyup = function (e) { 
-    let userGuess = e.key;
-    console.log(e.key);
-    console.log(wordSpot);
-    let computerGuess =
+function replaceAt(str, index, chr) {
+  if (index > str.length - 1) return str;
+  return str.substr(0) + chr + str.substr(index + 1);
+}
+
+console.log(wordChoices[0].replace(dash, wordChoices));
+
+document.onkeyup = function (e) {
+  const userGuess = e.key;
+  console.log(e.key);
+  console.log(wordSpot);
+  const computerGuess =
     wordChoices[Math.floor(Math.random() * wordChoices.length)];
-    console.log(computerGuess.length);
+  console.log(computerGuess.length);
 
-
-
-
-    wordSpot.textContent = computerGuess;
- }
+  wordSpot.textContent = computerGuess;
+};
