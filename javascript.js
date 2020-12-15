fa6760515577b9c5d49e4b870600e8ba2d16a682
joffrey <jaydru@gmail.com>
2020-12-15 15:07:00 -0700

    small edits

--- javascript.js
+++ javascript.js
@@ -1,38 +1,48 @@
 const wordChoices = [
-  'handstand',
-  'child pose',
-  'corpse',
-  'warrior',
-  'happy baby',
-  'frog',
-  'pigeon',
-  'upward dog',
-  'crane',
-  'bow',
-  'eagle',
-  'wheel',
-  'boat',
-  'plow',
-  'cow face',
+  'Handstand',
+  'Child Pose',
+  'Corpse',
+  'Warrior',
+  'Happy Baby',
+  'Frog',
+  'Pigeon',
+  'Upward Dog',
+  'Crane',
+  'Bow',
+  'Eagle',
+  'Wheel',
+  'Boat',
+  'Plow',
+  'Cow Face',
+  'Downward Dog',
+  'Bound Angle',
+  'Dolphin Plank',
+  'Extended Puppy',
+  'Feathered Peacock',
+  'Mariachi',
 ];
 const wordSpot = document.querySelector('#word-choice');
 const startBtn = document.querySelector('#start');
 const win = document.querySelector('#wins');
 const dash = '_';
+const lettersGuessed = [];
+const possibleGuesses = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-function startGame() {
-  console.log('did it work dues');
+console.log(wordChoices.length);
+
+function generateWord() {
+  const wordSelected =
+    wordChoices[Math.floor(Math.random() * wordChoices.length)];
+  for (let i = 0; i < wordSelected.length; i++) {
+    console.log(wordSelected);
+
+    const underscore = ' ';
+    const nextChar = wordSelected.charAt(i) === underscore ? underscore : '_';
+    console.log(nextChar);
+  }
 }
 
-console.log(wordChoices[4]);
-
-function replaceAt(str, index, chr) {
-  if (index > str.length - 1) return str;
-  return str.substr(0) + chr + str.substr(index + 1);
-}
-
-console.log(wordChoices[0].replace(dash, wordChoices));
-
+// adds word to display
 document.onkeyup = function (e) {
   const userGuess = e.key;
   console.log(e.key);
@@ -40,6 +50,19 @@ document.onkeyup = function (e) {
   const computerGuess =
     wordChoices[Math.floor(Math.random() * wordChoices.length)];
   console.log(computerGuess.length);
+
+  for (let i = 0; i < computerGuess.length; i++) {
+    console.log(computerGuess.length);
+    const wordLength = computerGuess.length;
+    const wordArray = [];
+    console.log(wordLength);
+    console.log(typeof wordLength);
+    const dash = '_';
+
+    computerGuess.length.textContent = dash;
+    console.log(dash.length);
+    break;
+  }
 
   wordSpot.textContent = computerGuess;
 };
