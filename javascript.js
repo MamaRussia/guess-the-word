d2e3eb144ca71dfefe42251b216d4cd41c784ee8
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2020-12-22 14:09:51 -0700

    changed logic added buttons + bootstrap

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const wordChoices = [
   'Handstand',
   'Child Pose',
@@ -22,61 +23,128 @@ const wordChoices = [
   'Mariachi',
 ];
 const startBtn = document.querySelector('.start');
-const win = document.querySelector('#wins');
+let mistakes = 0;
 const lettersGuessed = [];
-let word = [];
-const secretWord = '';
-let wordArea = '';
-const arrayWord = [];
+const word = '';
+let answer = '';
+const wordArea = '';
+const maxWrong = 6;
+const wordStatus = null;
 
 function randomWord() {
-  return wordChoices[Math.floor(Math.random() * wordChoices.length)];
+   answer = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 }
 
-function addWord(v) {
-  console.log(word);
-  wordArea = `${v}${wordArea}`;
-  const guessesLeft = word.length;
-  console.log(guessesLeft);
+// function addWord(v) {
+//   wordArea = `${v}${wordArea}`;
+//   console.log(wordArea);
+// }
+
+// function addDash() {
+//   for (let i = 0; i < word.length; i++) {
+//     console.log(word);
+//     // arrayWord.push('_ ');
+//   }
+
+//   arrayWord.forEach(addWord);
+//   document.querySelector('.word-choice').innerHTML = wordArea;
+//   console.log(typeof wordArea);
+// }
+
+function updateMistakes() {
+  document.getElementById('mistakes').innerHTML = mistakes;
 }
 
-function addDash() {
-  for (let i = 0; i < word.length; i++) {
-    arrayWord.push('_ ');
+function checkIfWon() {
+  if (wordStatus === answer) {
+    document.getElementById('keyboard').innerHTML = 'Namaste';
   }
-  console.log(arrayWord);
+}
 
-  arrayWord.forEach(addWord);
-  document.querySelector('.word-choice').innerHTML = wordArea;
+function checkIfLost() {
+  if (mistakes === maxWrong) {
+    document.getElementById(
+      'wordSpotlight'
+    ).innerHTML = `The answer was: ${answer}`;
+    document.getElementById('keyboard').innerHTML =
+      'The man is dead. Now what?';
+  }
+}
+
+function guessedWord() {
+  const wordStatus = answer
+    .split('')
+    .map((letter) =>
+      lettersGuessed.indexOf(letter) >= 0 ? lettersGuessed : ' _ '
+    )
+    .join('');
+
+  document.querySelector('.wordSpot').innerHTML = wordStatus;
 }
 
 function hideStart() {
   startBtn.style.display = 'none';
 }
 
-function splitWord() {
-  word = randomWord().split('');
-  addDash();
-  hideStart();
+function generateButtons() {
+  const buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'
+    .split('')
+    .map(
+      (letter) =>
+        `
+      <button
+        class="btn btn-lg btn-primary m-2"
+        id='${letter}'
+        onClick="handleGuess('${letter}')"
+      >
+        ${letter}
+      </button>
+    `
+    )
+    .join('');
+
+  document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
+function handleGuess(chosenLetter) {
+  lettersGuessed.indexOf(chosenLetter) === -1
+    ? lettersGuessed.push(chosenLetter)
+    : null;
+  document.getElementById(chosenLetter).setAttribute('disabled', true);
+
+  if (lettersGuessed.indexOf(chosenLetter) >= 0) {
+    guessedWord();
+    checkIfWon();
+  } else if (lettersGuessed.indexOf(chosenLetter) === -1) {
+    mistakes++;
+    updateMistakes();
+    checkIfLost();
+    // updateHangmanPicture();
+  }
 }
 
-function askLetter(letter) {
-  letter = letter.toUpperCase();
-  let guessesLeft = word.length;
-  console.log(guessesLeft);
+randomWord();
+generateButtons();
+guessedWord();
 
-  if (lettersGuessed.indexOf(letter) > -1) {
-    return;
-  }
+// // track key user presses
+// function keyPressed() {
+//   document.onkeyup = function (e) {
+//     const guess = e.key;
+//     randomWord();
+//     generateButtons();
+//     guessedWord();
+//   };
+// }
 
-  lettersGuessed.push(letter);
-  const correct = secretWord.indexOf(letter) > -1;
+// // function splitWord() {
+// //   word = randomWord().split('');
+// //   addDash();
+// //   hideStart();
+// //   keyPressed();
+// //   // console.log(word[0]);
+// //   // console.log(typeof word);
+// //   // console.log(arrayWord);
+// //   // console.log(guess);
+// // }
 
-  if (!correct) {
-    guessesLeft -= 1;
-  }
-
-  return correct;
-}
-
-startBtn.addEventListener('mousedown', splitWord);
+// startBtn.addEventListener('mousedown', keyPressed);
