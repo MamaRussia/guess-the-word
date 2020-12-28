cfc8aa618684508ce5a83eb1d3d18bd90c224c04
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2020-12-28 08:12:34 -0700

    responsive img

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// const express = require('express');
-
-// const PORT = process.env.PORT || '8080';
-// const app = express();
-// app.set('port', PORT);
-// // app.listen(process.env.PORT || 3000, function () {
-// //   console.log(
-// //     'Express server listening on port %d in %s mode',
-// //     this.address().port,
-// //     app.settings.env
-// //   );
-// // });
-
 const wordChoices = [
   'handstand',
   'headstand',
